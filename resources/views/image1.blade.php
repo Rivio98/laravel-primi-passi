4eb745a8a4780bd36928ce3988f5b25cb9c4dde4
Rivio98 <rivaldogjoni1998@gmail.com>
2024-09-23 17:23:38 +0200

    [UPD]Add links with img and bonus completed

--- resources/views/image1.blade.php
+++ resources/views/image1.blade.php
@@ -7,29 +7,19 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Laravel</title>
+    <title>Document</title>
 </head>
 
-<body class="bg-dark">
-    <div class="container bg-light mt-5">
-        <div class="row">
-            <div class="col-12">
-                <div class="content text-center py-3">
-                    <h1 class="text-primary">{{ $message }}</h1>
-                </div>
-            </div>
-        </div>
-    </div>
+<body>
     <div class="container mt-5">
         <div class="row">
             <div class="col-12">
-                <div class="content pt-3">
+                <div class="content">
                     <nav class="navbar navbar-expand-lg navbar-light bg-light">
                         <div class="collapse navbar-collapse" id="navbarNav">
                             <ul class="navbar-nav d-flex justify-content-evenly" style="display: flex; width: 100%;">
                                 <li class="nav-item active">
-                                    <h2><a class="nav-link" href="{{ route('image1') }}">Test
-                                            1</a></h2>
+                                    <h2><a class="nav-link" href="{{ route('home') }}">Home</a></h2>
                                 </li>
                                 <li class="nav-item">
                                     <h2><a class="nav-link" href="{{ route('image2') }}">Test 2</a></h2>
@@ -41,11 +31,11 @@
                         </div>
                     </nav>
                 </div>
+                <div class="content d-flex justify-content-center">
+                    <img src='{{ $img1 }}' alt="">
+                </div>
             </div>
         </div>
-    </div>
-    </div>
-    </div>
     </div>
 </body>
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Faker\Guesser\Name;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
-});
+    $message = 'Hello Laravel';
+    return view('home', compact('message'));
+})->name('home');
+
+Route::get('/img1', function () {
+    $img1 = 'https://static.vecteezy.com/system/resources/thumbnails/022/660/007/small_2x/generative-ai-illustration-of-gaming-background-abstract-cyberpunk-style-of-gamer-wallpaper-neon-glow-light-of-scifi-fluorescent-sticks-digitally-generated-image-photo.jpeg';
+    return view('image1', compact('img1'));
+})->name('image1');
+
+
+Route::get('/img2', function () {
+    $img2 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJ59oQN0Ecwudpp9b3e9ngFAY0jO1u4igoeEUf_QaOV9DihaBoqOz3gKmT52laBV8aYx8&usqp=CAU';
+    return view('image2', compact('img2'));
+})->name('image2');
+
+Route::get('/img3', function () {
+    $img3 = 'https://static.vecteezy.com/system/resources/thumbnails/022/697/266/small_2x/generative-ai-illustration-of-gaming-background-abstract-cyberpunk-style-of-gamer-wallpaper-neon-glow-light-of-scifi-fluorescent-sticks-digitally-generated-image-photo.jpeg';
+    return view('image3', compact('img3'));
+})->name('image3');
