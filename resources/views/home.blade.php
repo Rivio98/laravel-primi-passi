d4930c26a65a38023a3268701f112793c66028b3
Rivio98 <rivaldogjoni1998@gmail.com>
2024-09-23 16:29:53 +0200

    [UPD]Add style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,15 +10,41 @@
     <title>Laravel</title>
 </head>
 
-<body>
-    <div class="container">
+<body class="bg-dark">
+    <div class="container bg-light mt-5">
         <div class="row">
             <div class="col-12">
-                <div class="content text-center pt-5">
+                <div class="content text-center py-3">
                     <h1 class="text-primary">Hello Laravel</h1>
                 </div>
             </div>
         </div>
+    </div>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-12">
+                <div class="content pt-3">
+                    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav d-flex justify-content-evenly" style="display: flex; width: 100%;">
+                                <li class="nav-item active">
+                                    <h2><a class="nav-link" href="#">Test 1</a></h2>
+                                </li>
+                                <li class="nav-item">
+                                    <h2><a class="nav-link" href="#">Test 2</a></h2>
+                                </li>
+                                <li class="nav-item">
+                                    <h2><a class="nav-link" href="#">Test 3</a></h2>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    </div>
     </div>
 </body>
 
